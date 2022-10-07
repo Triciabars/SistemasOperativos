ceba01bc1f65a587ad2ae4b1c4664431b9de91c1
Triciabars <pplata@ucm.es>
2022-10-07 23:43:34 +0200

    archivos

---
+++ Práctica 2/2. Procesos e hilos/Ejercicio 2/fork5.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* programa que crea N hijos siguiendo un grado de dependencias. Usa las
+ * versiones de execl y execlp */
+
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}

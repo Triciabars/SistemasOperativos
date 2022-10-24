2e965ced2f570b3187ed6ee3cf26c49a3f0ca2c0
justbreathe23 <miriamgarciagodoy23@gmail.com>
2022-10-24 18:06:43 +0200

    cambio de nuevo 2.2.1

--- Práctica 2/2. Procesos e hilos/Ejercicio 1/fork1.c
+++ Práctica 2/2. Procesos e hilos/Ejercicio 1/fork1.c
@@ -9,36 +9,40 @@
 int main(int argc, char **argv){
 	pid_t pid, pid_h;
 	int status_code;
+	char **args;
+	
+	//get argumentos
+	args=&argv[1]; //coger todos los parámetros a partir de la llamada
 
 	pid = fork(); //se crea el hijo
 
 	if(pid==0){ //proceso hijo
 		printf("Soy el proceso hijo con PID %d. Mi padre es %d Global\n", getpid(), getppid());
-		pid_h=fork(); //se hace la segunda llamada al fork y se vuelve a crear un hijo
+	}	
+	else{ //proceso padre
+	printf("Soy el proceso padre con PID %d. Mi padre es %d Global\n", getpid(), getppid());
+	wait(&status); //el proceso padre espera a que termine el hijo
+	}
+
+	pid_h=fork(); //se hace la segunda llamada al fork y se vuelve a crear un hijo
 		
 		if(pid_h==0){ //si es el hijo del prcoeso hijo
 			printf("Soy el proceso hijo con PID %d. Mi padre es %d Global\n", getpid(), getppid());
-		
+		}
 		else{ //padre del proceso hijo
 			printf("Soy el proceso padre con PID %d. Mi padre es %d Global\n", getpid(), getppid());
 			//pid_t waitpid(pid_t pid, int *status, int options);
-			if(waitpid(getpid, &status_code, WNOHANG) == -1){ //esperar al pid concreto
-					perror("waitpid");
-					exit(EXIT_FAILURE);
-				}
+			if(waitpid(getpid(), &status_code, WNOHANG) == -1){ //esperar al pid concreto
+				perror("waitpid");
+				exit(EXIT_FAILURE);
 			}
-		}	//int execvp(const char* command, char* argv[]);
-		status_code = execvp(argv, argv);
+		}
+		//int execvp(const char* command, char* argv[]);
+		status_code = execvp(argv[0], *args);
 		if (status_code == -1) {
 			printf("Process did not terminate correctly\n");
 			exit(1);
 		}
-	}
-	
-	else{ //proceso padre
-	printf("Soy el proceso padre con PID %d. Mi padre es %d Global\n", getpid(), getppid());
-	wait(NULL); //el proceso padre espera a que termine el hijo
-	}
 
 	return 0;
 }

dc7585d6da29d8c4113cf999a03f67cb199ec93e
Triciabars <pplata@ucm.es>
2022-10-27 14:04:02 +0200

    comentarios

--- Práctica 2/2. Procesos e hilos/Ejercicio 3/hilos.c
+++ Práctica 2/2. Procesos e hilos/Ejercicio 3/hilos.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
-void *thread_usuario(void *arg)
+void *thread_usuario(void *arg) //puntero con estructura de dos campos (entero y caracter)
 {
-
+//usando memoria dinamica crear variable
 }
 
 int main(int argc, char* argv[])
@@ -11,3 +12,4 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+//crea un hilo por cada usuario

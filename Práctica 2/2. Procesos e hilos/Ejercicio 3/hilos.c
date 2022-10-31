5a9c4ee425cadbbcece49e0962e3252d5f4c41cd
Triciabars <pplata@ucm.es>
2022-10-31 18:48:14 +0100

    hilos mejorado

--- Práctica 2/2. Procesos e hilos/Ejercicio 3/hilos.c
+++ Práctica 2/2. Procesos e hilos/Ejercicio 3/hilos.c
@@ -1,29 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#define N 5
+typedef struct {
+	int id_usuario;
+	int prioridad; //char
+} usuario;
 
-void *thread_usuario(void *arg) //puntero con estructura de dos campos (entero y caracter)
+void *thread_usuario(void *arg) 
 {
-	//usando memoria dinamica crear variable
-	//Duda si mostrarlo aquí, pero no lo paso?
-	printf (" Thread % ld \n " ,( long unsigned int) pthread_self ());
-	printf(arg[0],arg[1]);//error?
-	pthread_exit (0) ;
+	
+	usuario usuario_local = *((usuario *) arg);
+	free(arg);
+	printf (" Thread %d, %d,%d \n",pthread_self(), usuario_local.id_usuario, usuario_local.prioridad);
+	
 }
 
 int main(int argc, char* argv[])
 {
 	//Inicializar y crear memoria dinámica, no estoy segura si aquí o en la función
-	pthread_t *pthr_arr_usuarios;
-	int* num_user=(int*)malloc(sizeof(int));
-	char* priority= (char*)malloc(sizeof(char));
-	pthr_arr_usuarios = (pthread_t *)malloc(sizeof(pthread_t));
+	pthread_t usuarios[N];
 
-	num_user=argv[0];
-	priority=argv[1];//lo de impares y pares no sé cómo funcionaría aquí
 	//Creamos el hilo
-	for (int i = 0; i < (argc/2); i++)
-	pthread_create (&pthr_arr_usuarios[i], NULL,thread_usuario,NULL);//duda no paso argumentos?
+	for (int i = 1; i =< N; i++){
+	usuario *arg = (usuario *) malloc(sizeof(usuario));
+	arg->id_usuario = i;
+	arg->prioridad = i%2;
+	pthread_create (&usuarios[i-1], NULL,thread_usuario, arg);
+	}
+	for (int i = 1; i =< N; i++){
+		pthread_join(&usuarios[i-1], NULL);
+	}
+
 	return 0;
 }
-//crea un hilo por cada usuario

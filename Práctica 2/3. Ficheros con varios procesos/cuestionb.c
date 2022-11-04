42488fe04da8c7d88f18d03f16426346a00038a8
Triciabars <pplata@ucm.es>
2022-11-04 20:15:52 +0100

    correcciones

--- Práctica 2/3. Ficheros con varios procesos/cuestionb.c
+++ Práctica 2/3. Ficheros con varios procesos/cuestionb.c
@@ -12,20 +12,20 @@ int main(void)
     char c;
     char buffer[6];
 
-    fd1 = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+    fd1 = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR); //abre fd1
     for (i=1; i < 10; i++) {
-        pos = lseek(fd1, 0, SEEK_CUR);
-        if (fork() == 0) {
+        pos = lseek(fd1, 0, SEEK_CUR); //puntero se coloca en donde estaba +0
+        if (fork() == 0) { //crea hijo y hace lo de detro si ya es hijo
             /* Child */
             sprintf(buffer, "%d", i*11111);
-            lseek(fd1, pos, SEEK_SET);
+            lseek(fd1, pos, SEEK_SET); //puntero de fd1 se posiciona en pos
             write(fd1, "00000", 5);
             write(fd1, buffer, 5);
             close(fd1);
-            exit(0);
-        } else {
+            exit(0); 
+        } else { //padre hace lo siguiente
             /* Parent */
-            lseek(fd1, 10, SEEK_CUR);
+            lseek(fd1, 10, SEEK_CUR); //puntero se posiciona en donde estaba más 10
         }
     }
 

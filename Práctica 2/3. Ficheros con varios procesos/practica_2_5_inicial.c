2901003c9a600a890fb8bc3601509a8a6d8e8453
Triciabars <pplata@ucm.es>
2022-11-04 19:37:27 +0100

    maq vir

--- Práctica 2/3. Ficheros con varios procesos/practica_2_5_inicial.c
+++ Práctica 2/3. Ficheros con varios procesos/practica_2_5_inicial.c
@@ -12,17 +12,16 @@ int main(void)
     char c;
     char buffer[6];
 
-    fd1 = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR); //cuidado comparten punteros
+    fd1 = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
     write(fd1, "00000", 5);
     for (i=1; i < 10; i++) {
         pos = lseek(fd1, 0, SEEK_CUR);
         if (fork() == 0) {
             /* Child */
-            fd2 = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
             sprintf(buffer, "%d", i*11111);
-            lseek(fd2, pos, SEEK_SET);
-            write(fd2, buffer, 5);
-            close(fd2);
+            lseek(fd1, pos, SEEK_SET);
+            write(fd1, buffer, 5);
+            close(fd1);
             exit(0);
         } else {
             /* Parent */

b9fcd917f0cb8e7e9fe928ae5aa060b0117bf218
justbreathe23 <miriamgarciagodoy23@gmail.com>
2022-11-21 18:50:31 +0100

    finiquitado 2.2.1

--- Práctica 2/2. Procesos e hilos/Ejercicio 1/fork1.c
+++ Práctica 2/2. Procesos e hilos/Ejercicio 1/fork1.c
@@ -20,29 +20,32 @@ int main(int argc, char **argv){
 		printf("Soy el proceso hijo con PID %d. Mi padre es %d Global\n", getpid(), getppid());
 	}	
 	else{ //proceso padre
-	printf("Soy el proceso padre con PID %d. Mi padre es %d Global\n", getpid(), getppid());
-	wait(&status); //el proceso padre espera a que termine el hijo
+		printf("Soy el proceso padre con PID %d. Mi padre es %d Global\n", getpid(), getppid());
+		wait(NULL); //el proceso padre espera a que termine el hijo
 	}
 
 	pid_h=fork(); //se hace la segunda llamada al fork y se vuelve a crear un hijo
 		
 		if(pid_h==0){ //si es el hijo del prcoeso hijo
 			printf("Soy el proceso hijo con PID %d. Mi padre es %d Global\n", getpid(), getppid());
+			status_code = execvp(argv[0], *args);
+			if (status_code == -1) {
+				printf("Process did not terminate correctly\n");
+				exit(1);
+			}
 		}
 		else{ //padre del proceso hijo
+			wait(NULL);
 			printf("Soy el proceso padre con PID %d. Mi padre es %d Global\n", getpid(), getppid());
 			//pid_t waitpid(pid_t pid, int *status, int options);
-			if(waitpid(getpid(), &status_code, WNOHANG) == -1){ //esperar al pid concreto
-				perror("waitpid");
-				exit(EXIT_FAILURE);
+			status_code = execvp(argv[0], *args);
+			if (status_code == -1) {
+				printf("Process did not terminate correctly\n");
+				exit(1);
 			}
 		}
 		//int execvp(const char* command, char* argv[]);
-		status_code = execvp(argv[0], *args);
-		if (status_code == -1) {
-			printf("Process did not terminate correctly\n");
-			exit(1);
-		}
+		
 
 	return 0;
 }

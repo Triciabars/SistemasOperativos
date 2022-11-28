92abbdaa287f82ecd0d704532b282da65bae9eb9
Triciabars <pplata@ucm.es>
2022-11-28 12:20:25 +0100

    mejoras (lo he hecho con semaforos no sé si está bien xq no he usado sinint y tal las señales esas)

--- Práctica 3/Ejercicio 2/cocinero.c
+++ Práctica 3/Ejercicio 2/cocinero.c
@@ -3,11 +3,16 @@
 #include <unistd.h>
 
 #define M 10
-
+int caldero = 0;
+sem_t m, empty, full;
 int finish = 0;
 
 void putServingsInPot(int servings)
 {
+	sem_wait(&empty);
+	caldero += raciones;
+	printf("Llenando caldero %d\n", caldero);
+	sem_post(&full);
 }
 
 void cook(void)
@@ -24,6 +29,20 @@ void handler(int signo)
 
 int main(int argc, char *argv[])
 {
+	int i;
+	pthread_t tid_cook;
+
+	sem_init(&m, 0, 1);
+	sem_init(&empty, 0, 0);
+	sem_init(&full, 0, 0);
+	
+	pthread_create(&tid_cook, NULL, cocinero, NULL);
+	
+	pthread_join(tid_cook, NULL);
+	
+	sem_destroy(&m);
+	sem_destroy(&empty);
+	sem_destroy(&full);
 
 	return 0;
 }
--- Práctica 3/Ejercicio 2/salvajes.c
+++ Práctica 3/Ejercicio 2/salvajes.c
@@ -3,10 +3,22 @@
 #include <unistd.h>
 
 #define NUMITER 3
+int caldero = 0;
+sem_t m, empty, full;
 
 int getServingsFromPot(void)
 {
-
+	unsigned long id = (unsigned long) getpid();
+	sem_wait(&m);
+	if (caldero == 0) {
+		printf("Salvaje %d: caldero vacío\n", id);
+		sem_post(&empty);
+		sem_wait(&full);
+	}
+	printf("Salvaje %d sirviendo ración del caldero %d\n",
+			id, caldero);
+	caldero--;
+	sem_post(&m);
 }
 
 void eat(void)
@@ -18,11 +30,36 @@ void eat(void)
 
 void savages(void)
 {
-
+	while(1) {
+		getServingsFromPot();
+		eat();
+	}
 }
 
 int main(int argc, char *argv[])
 {
+	int i;
+	pthread_t *tid;
 
+	tid = malloc(NUMITER * sizeof(pthread_t));
+	if (tid == NULL){
+		perror("malloc tid");
+		exit(EXIT_FAILURE);
+	}
+
+	sem_init(&m, 0, 1);
+	sem_init(&empty, 0, 0);
+	sem_init(&full, 0, 0);
+
+	for (i = 0; i < NUMITER; i++)
+		pthread_create(&tid[i], NULL, savages, (void*) i);
+
+	for (i = 0; i < n; i++)
+		pthread_join(tid[i], NULL);
+
+	
+	sem_destroy(&m);
+	sem_destroy(&empty);
+	sem_destroy(&full);
 	return 0;
 }

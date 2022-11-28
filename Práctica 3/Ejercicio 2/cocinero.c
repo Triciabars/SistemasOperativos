0f517a9fbaa74c54af8c4448904745502767be70
Triciabars <pplata@ucm.es>
2022-11-28 18:27:37 +0100

    holaa

--- Práctica 3/Ejercicio 2/cocinero.c
+++ Práctica 3/Ejercicio 2/cocinero.c
@@ -8,15 +8,15 @@
 
 #define M 10
 int *caldero;
-sem_t m, empty, full;
+sem_t *m, *empty, *full;
 int finish = 0;
 
 void putServingsInPot(int servings)
 {
-	sem_wait(&empty);
+	sem_wait(empty);
 	*caldero += servings;
 	printf("Llenando caldero %d\n", *caldero);
-	sem_post(&full);
+	sem_post(full);
 }
 
 void cook(void)
@@ -34,9 +34,9 @@ void handler(int signo)
 int main(int argc, char *argv[])
 {
 	int i, shd;
-	*m = sem_open("/MUTEX", O_CREAT|O_RDWR, 0700,1);
-	*empty = sem_open("/EMPTY", O_CREAT|O_RDWR, 0700, 1);
-	*full = sem_open("/FULL", O_CREAT|O_RDWR, 0700,0);
+	m = sem_open("/MUTEX", O_CREAT|O_RDWR, 0700,1);
+	empty = sem_open("/EMPTY", O_CREAT|O_RDWR, 0700, 1);
+	full = sem_open("/FULL", O_CREAT|O_RDWR, 0700,0);
 	shd = shm_open("/CALDERO", O_CREAT|O_EXCL|O_RDWR, S_ISUSR | S_IWUSR); //CON OPEN() TAMBIEN DIAPO 63
 	ftruncate(shd, sizeof(int));
 	caldero = (int*) mmap(NULL, M * sizeof(int), PROT_WRITE|PROT_READ, MAP_SHARED, shd, 0);
@@ -46,9 +46,9 @@ int main(int argc, char *argv[])
 	
 	munmap(caldero, M * sizeof(int));
 	close(shd);
-	sem_close(&m);
-	sem_close(&empty);
-	sem_close(&full);
+	sem_close(m);
+	sem_close(empty);
+	sem_close(full);
 	sem_unlink("/MUTEX");
 	sem_unlink("/EMPTY");
 	sem_unlink("/FULL");
--- Práctica 3/Ejercicio 2/salvajes.c
+++ Práctica 3/Ejercicio 2/salvajes.c
@@ -8,21 +8,21 @@
 
 #define NUMITER 3
 int *caldero;
-sem_t m, empty, full;
+sem_t *m, *empty, *full;
 
 int getServingsFromPot(void)
 {
 	unsigned long id = (unsigned long) getpid();
-	sem_wait(&m);
+	sem_wait(m);
 	if (*caldero == 0) {
 		printf("Salvaje %d: caldero vacío\n", id);
-		sem_post(&empty);
-		sem_wait(&full);
+		sem_post(empty);
+		sem_wait(full);
 	}
 	printf("Salvaje %d sirviendo ración del caldero %d\n",
 			id, *caldero);
 	*caldero--;
-	sem_post(&m);
+	sem_post(m);
 }
 
 void eat(void)
@@ -44,9 +44,9 @@ int main(int argc, char *argv[])
 {
 	int i, shd;
 
-	*m = sem_open("/MUTEX", O_CREAT|O_RDWR, 0700,1);
-	*empty = sem_open("/EMPTY", O_CREAT|O_RDWR, 0700, 1);
-	*full = sem_open("/FULL", O_CREAT|O_RDWR, 0700,0);
+	m = sem_open("/MUTEX", O_CREAT|O_RDWR, 0700,1);
+	empty = sem_open("/EMPTY", O_CREAT|O_RDWR, 0700, 1);
+	full = sem_open("/FULL", O_CREAT|O_RDWR, 0700,0);
 	shd = shm_open("/CALDERO", O_CREAT|O_EXCL|O_RDWR, S_ISUSR | S_IWUSR); //CON OPEN() TAMBIEN DIAPO 63
 	ftruncate(shd, sizeof(int));
 	caldero = (int*) mmap(NULL, M * sizeof(int), PROT_WRITE|PROT_READ, MAP_SHARED, shd, 0);
@@ -55,9 +55,9 @@ int main(int argc, char *argv[])
 
 	munmap(caldero, M * sizeof(int));
 	close(shd);
-	sem_close(&m);
-	sem_close(&empty);
-	sem_close(&full);
+	sem_close(m);
+	sem_close(empty);
+	sem_close(full);
 	sem_unlink("/MUTEX");
 	sem_unlink("/EMPTY");
 	sem_unlink("/FULL");

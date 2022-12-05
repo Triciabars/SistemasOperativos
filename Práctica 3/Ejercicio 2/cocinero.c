b04e9e1af5d7d33a33abb944054af8f9062a8ca4
Triciabars <pplata@ucm.es>
2022-12-05 11:27:38 +0100

    mejoras profe

--- Práctica 3/Ejercicio 2/cocinero.c
+++ Práctica 3/Ejercicio 2/cocinero.c
@@ -6,6 +6,10 @@
 #include <sys/mman.h>
 #include <signal.h>
 
+#include <pthread.h>
+#include <err.h>
+#include <sys/stat.h>
+
 #define M 10
 int *caldero;
 sem_t *m, *empty, *full;
@@ -14,10 +18,8 @@ int finish = 0;
 void putServingsInPot(int servings)
 {
 	sem_wait(empty);
-	sem_wait(m);
 	*caldero += servings;
 	printf("Llenando caldero %d\n", *caldero);
-	sem_post(m);
 	sem_post(full);
 }
 
@@ -30,25 +32,43 @@ void cook(void)
 
 void handler(int signo)
 {
-	finish = signo;
+	finish = 1;
 }
 
 int main(int argc, char *argv[])
 {
+	// Initialize
 	int i, shd;
-	m = sem_open("/MUTEX", O_CREAT|O_RDWR, 0700,1);
-	empty = sem_open("/EMPTY", O_CREAT|O_RDWR, 0700, 1);
-	full = sem_open("/FULL", O_CREAT|O_RDWR, 0700,0);
+	struct sigaction sa;
+	sigset_t set;
+	
+	// Handler
+	sigfillset(&set);
+	sa.sa_mask = set;
+	sa.sa_flags = 0;
+	sa.sa_handler = handler;
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGINT, &sa, NULL);
+	
+	// Create semmaphores
+	m = sem_open("/MUTEX", O_CREAT, 0700,1);
+	empty = sem_open("/EMPTY", O_CREAT, 0700, 1);
+	full = sem_open("/FULL", O_CREAT, 0700,0);
+	
+	// Create mmap
 	shd = shm_open("/CALDERO", O_CREAT|O_EXCL|O_RDWR, S_IRUSR | S_IWUSR); //CON OPEN() TAMBIEN DIAPO 63
 	ftruncate(shd, sizeof(int));
-	caldero = (int*) mmap(NULL, M * sizeof(int), PROT_WRITE|PROT_READ, MAP_SHARED, shd, 0);
+	// Apply mmap
+	caldero = (int*) mmap(NULL, sizeof(int), PROT_WRITE|PROT_READ, MAP_SHARED, shd, 0);
 	*caldero = 0;
 	
+	// Run main cook program
 	cook();
 	
-	munmap(caldero, M * sizeof(int));
-	close(shd);
+	// Empty everything
+	munmap(caldero, sizeof(int));
 	shm_unlink("/CALDERO");
+	close(shd);
 	sem_close(m);
 	sem_close(empty);
 	sem_close(full);
--- Práctica 3/Ejercicio 2/salvajes.c
+++ Práctica 3/Ejercicio 2/salvajes.c
@@ -6,26 +6,29 @@
 #include <sys/mman.h>
 #include <signal.h>
 
-#define NUMITER 3
+#define NUMITER 15
 int *caldero;
 sem_t *m, *empty, *full;
 
 int getServingsFromPot(void)
 {
+	unsigned long id = (unsigned long) getpid();
 	sem_wait(m);
 	if (*caldero == 0) {
-		printf("Salvaje: caldero vacío\n");
+		printf("Salvaje %lu: caldero vacío\n", id);
 		sem_post(empty);
 		sem_wait(full);
 	}
-	printf("Salvaje sirviendo ración del caldero %d\n", *caldero);
-	*caldero--;
+	printf("Salvaje %lu sirviendo ración del caldero %d\n",
+			id, *caldero);
+	(*caldero)--;
 	sem_post(m);
 }
 
 void eat(void)
 {
-	printf("Savage eating\n");
+	unsigned long id = (unsigned long) getpid();
+	printf("Savage %lu eating\n", id);
 	sleep(rand() % 5);
 }
 
@@ -33,24 +36,27 @@ void savages(void)
 {
 	for(int i=0; i<NUMITER; i++) {
 		getServingsFromPot();
+		eat();
 	}
-	eat();
 }
 
 int main(int argc, char *argv[])
 {
 	int i, shd;
 
+	// Load already created semaphores
 	m = sem_open("/MUTEX", 0);
 	empty = sem_open("/EMPTY", 0);
 	full = sem_open("/FULL", 0);
-	shd = shm_open("/CALDERO", O_CREAT|O_EXCL|O_RDWR, S_IRUSR | S_IWUSR); //CON OPEN() TAMBIEN DIAPO 63
-	ftruncate(shd, sizeof(int));
-	caldero = (int*) mmap(NULL, 10 * sizeof(int), PROT_WRITE|PROT_READ, MAP_SHARED, shd, 0);
 	
+	// Load mmap 
+	shd = shm_open("/CALDERO", O_RDWR, 0); 
+	caldero = (int*) mmap(NULL, sizeof(int), PROT_WRITE|PROT_READ, MAP_SHARED, shd, 0);
+	
+	// Run main program
 	savages();
 	
-	munmap(caldero, 10 * sizeof(int));
+	// Close everything
 	close(shd);
 	sem_close(m);
 	sem_close(empty);
